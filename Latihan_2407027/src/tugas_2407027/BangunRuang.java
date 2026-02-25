91c49db670fa9234766592c0160bbc69bc94544c
Dela Ayu Regita <delaayuregita13@gmail.com>
2026-02-25 23:13:04 +0700

    tugas bangun ruang

--- Latihan_2407027/src/tugas_2407027/BangunRuang.java
+++ Latihan_2407027/src/tugas_2407027/BangunRuang.java
@@ -1,13 +1,13 @@
 package tugas_2407027;
 
 public class BangunRuang {
-    private double sisi, panjang, lebar, r; // Tambah r untuk lingkaran
+    private double sisi, panjang, lebar, r; 
     protected double tinggi;
     public int alas;
 
     // Setter & Getter
     public void setSisi(double sisi) { this.sisi = sisi; }
-    public void setR(double r) { this.r = r; } // Setter Jari-jari
+    public void setR(double r) { this.r = r; } 
     public void setData(double p, double l, double t, int a) {
         this.panjang = p; this.lebar = l; this.tinggi = t; this.alas = a;
     }
@@ -18,7 +18,7 @@ public class BangunRuang {
     public double luasSegitiga() { return 0.5 * alas * tinggi; }
     public double luasLingkaran() { return 3.14 * r * r; }
 
-    // Method Keliling (BARU)
+    // Method Keliling 
     public double kelPersegi() { return 4 * sisi; }
     public double kelPPanjang() { return 2 * (panjang + lebar); }
     public double kelLingkaran() { return 2 * 3.14 * r; }
